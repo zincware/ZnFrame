96e09fc10a78d87ed9305fc6701132758e24f110
Paul Hohenberger <49005843+phohenberger@users.noreply.github.com>
2024-01-19 18:08:08 +0100

    Another try to fix test

--- tests/test_znframe.py
+++ tests/test_znframe.py
@@ -57,7 +57,7 @@ def test_frame_to_dict(water):
         "connectivity": water.connectivity,
         "arrays": water.arrays,
         "info": water.info,
-        "calc": water.calc.results,
+        "calc": water.calc,
         "pbc": water.pbc,
         "cell": water.cell,
     }
